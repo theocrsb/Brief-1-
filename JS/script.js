1c15dba5ff9f66f95f33db5d2142553b4aab428b
Theocrsb <theo_crosbie@yahoo.fr>
2022-09-08 22:08:47 +0200

    Ajout d'un fichier js linker en bas du body dans le fichier html et ajout de la fonctionnalité pour cacher les listes avec un click coder en javascript

---
+++ JS/script.js
@@ -0,0 +1,48 @@
+// code a copier 4 fois pour 4 liste a cacher
+let zoneaclique = document.getElementById("zoneaclique");
+let listearetirer = document.getElementById("listearetirer");
+
+
+zoneaclique.addEventListener("click", () => {
+    if (getComputedStyle(listearetirer).display != "none") {
+        listearetirer.style.display = "none";
+    } else {
+        listearetirer.style.display = "flex";
+    }
+})
+// 2
+let zoneaclique2 = document.getElementById("zoneaclique2");
+let listearetirer2 = document.getElementById("listearetirer2");
+
+
+zoneaclique2.addEventListener("click", () => {
+    if (getComputedStyle(listearetirer2).display != "none") {
+        listearetirer2.style.display = "none";
+    } else {
+        listearetirer2.style.display = "flex";
+    }
+})
+// 3
+let zoneaclique3 = document.getElementById("zoneaclique3");
+let listearetirer3 = document.getElementById("listearetirer3");
+
+
+zoneaclique3.addEventListener("click", () => {
+    if (getComputedStyle(listearetirer3).display != "none") {
+        listearetirer3.style.display = "none";
+    } else {
+        listearetirer3.style.display = "flex";
+    }
+})
+// 4
+let zoneaclique4 = document.getElementById("zoneaclique4");
+let listearetirer4 = document.getElementById("listearetirer4");
+
+
+zoneaclique4.addEventListener("click", () => {
+    if (getComputedStyle(listearetirer4).display != "none") {
+        listearetirer4.style.display = "none";
+    } else {
+        listearetirer4.style.display = "flex";
+    }
+})
